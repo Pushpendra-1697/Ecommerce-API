c177800439856245a61b510527e16bb25adb3fe5
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-07-24 19:11:48 +0530

    added mongodb to store data

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-const express = require('express');
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-
+const http = require("http");
+const express = require("express");
 const app = express();
+const httpServer = http.createServer(app); //server creation by http inbuit node module;
+const { connection } = require("./Configs/Config");
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000; //defined port 8080 (default 3000) excluding 27017 (reserved port by Mongod);
+
+//Different Routers for different frontend pages in UI;
 
 
-app.listen(PORT , () => {
-    console.log(`Server is running at ${PORT}`);
+//Inbuilt middlewares;
+app.use(express.text());
+app.use(express.json());
+app.use(cors());
+
+//Landing/default route;
+app.get("/", async (req, res) => {
+    res.send("Welcome in Ecommerce-API App😊!!!");
+});
+
+//Fixed starting end points for making nested dynamic route;
+
+
+//server code for start or live my server at defined port;
+httpServer.listen(PORT, async () => {
+    try {
+        await connection;
+        console.log("connected to DB");
+    } catch (e) {
+        console.log({ message: e.message });
+    }
+    console.log(`Server is running at port ${PORT}`);
 });
